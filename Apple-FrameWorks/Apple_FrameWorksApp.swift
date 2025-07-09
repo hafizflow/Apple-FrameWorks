c9aa542f06d99bc0cb15ba63b26b17df50d02fdc
hafizflow <hafizur.rahman.cs@gmail.com>
2025-07-10 03:05:50 +0600

    Initial Commit

---
+++ Apple-FrameWorks/Apple_FrameWorksApp.swift
@@ -0,0 +1,17 @@
+//
+//  Apple_FrameWorksApp.swift
+//  Apple-FrameWorks
+//
+//  Created by Hafizur Rahman on 10/7/25.
+//
+
+import SwiftUI
+
+@main
+struct Apple_FrameWorksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Apple-FrameWorksTests/Apple_FrameWorksTests.swift
@@ -0,0 +1,17 @@
+//
+//  Apple_FrameWorksTests.swift
+//  Apple-FrameWorksTests
+//
+//  Created by Hafizur Rahman on 10/7/25.
+//
+
+import Testing
+@testable import Apple_FrameWorks
+
+struct Apple_FrameWorksTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
